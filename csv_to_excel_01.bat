73d86a62fa85880e7535d2ff6b3dc5394a74555d
caesaraugustus1738 <71141254+caesaraugustus1738@users.noreply.github.com>
2020-09-28 15:16:38 +0100

    Add files via upload
    BAT file for use on Windows. Drag and drop your CSV onto this BAT file. The BAT file will execute the PY script on this CSV file.

---
+++ csv_to_excel_01.bat
@@ -0,0 +1,4 @@
+rem Replace path with location of .py file.
+rem Drag and drop CSV onto this .bat file to run it.
+python "C:\Users\tullyg.TC\Documents\python\csv_to_excel_01\spreadsheet_test_02.py" %*
+PAUSE
